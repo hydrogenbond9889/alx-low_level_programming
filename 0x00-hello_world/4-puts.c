c084e0bb472b102361eaa82d417b788ab2a142a6
Gerald Omondi <luthergerry98@gmail.com>
2023-06-15 10:54:09 -0700

    just commiting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+/**
+      *hello world my new c program return 0
+      **/
 int main(void)
-{/**
-   *hello world my new c program
-   *return 0
-   **/
+{
 	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
